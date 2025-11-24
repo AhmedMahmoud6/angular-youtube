3d304fd1c25128ae64cd61ccb29daf1e12d2d791
Ahmed Mahmoud <ahmedmahmoudd626@gmail.com>
2025-11-24 17:16:52 +0200

    handled offline & errors + responsive

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  apiKey: 'AIzaSyDWsKhi_G5QGsBNKcCpXmf7ARoU19ues2M',
-  // apiKey: 'AIzaSyB59lAtfu3eg_SIB0LhE35agb5u7YJdkxU',
+  // apiKey: 'AIzaSyDWsKhi_G5QGsBNKcCpXmf7ARoU19ues2M',
+  apiKey: 'AIzaSyB59lAtfu3eg_SIB0LhE35agb5u7YJdkxU',
   videosUrl: `https://www.googleapis.com/youtube/v3`,
   channelUrl: 'https://www.googleapis.com/youtube/v3/channels'
 };
